9adf7a16018101d451daaa50fd5a9f523287534c
Mike Johnston <mrbean355@gmail.com>
2020-01-11 15:55:16 +0200

    Implement custom icons and text
    - TriHard chat wheel
    - BKB -> sadKEK Bar
    - Midas -> GorpPidas
    - Game is paused -> ResidentSleeper
    - Post-game survey options
    - Treant Protector -> Trent Protector of Minorities
    - Tip message: Well played -> Git gud scrub sadKEK

--- compile.bat
+++ compile.bat
@@ -1,11 +1,19 @@
-mkdir compile\pak01_dir
-
+REM Compile resources:
 ..\..\..\game\bin\win64\resourcecompiler -r -i sounds\*
+..\..\..\game\bin\win64\resourcecompiler -r -i panorama\*
 
+REM Copy resources to the 'compile' directory:
+mkdir compile\pak01_dir
 cp -r ..\..\..\game\dota_addons\admiralbulldog\sounds compile\pak01_dir\
+cp -r ..\..\..\game\dota_addons\admiralbulldog\panorama compile\pak01_dir\
+cp -r resource compile\pak01_dir\
 
+REM Build the VPK file:
 compile\vpk compile\pak01_dir
-
 mv compile\pak01_dir.vpk .
 
+REM Clean up:
 rmdir /s /q compile\pak01_dir
+rmdir /s /q ..\..\..\game\dota_addons\admiralbulldog
+
+PAUSE
