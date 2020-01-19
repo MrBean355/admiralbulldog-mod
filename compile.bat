6980afcaec85e5db678ae03d7c0d7922a50d336e
Mike Johnston <mrbean355@gmail.com>
2020-01-19 10:16:35 +0200

    Implement TreeHard talent tree

--- compile.bat
+++ compile.bat
@@ -8,6 +8,9 @@ cp -r ..\..\..\game\dota_addons\admiralbulldog\sounds compile\pak01_dir\
 cp -r ..\..\..\game\dota_addons\admiralbulldog\panorama compile\pak01_dir\
 cp -r resource compile\pak01_dir\
 
+REM Rename some resources:
+mv compile\pak01_dir\panorama\images\hud\reborn\statbranch_button_bg_png.vtex_c compile\pak01_dir\panorama\images\hud\reborn\statbranch_button_bg_psd.vtex_c
+
 REM Build the VPK file:
 compile\vpk compile\pak01_dir
 mv compile\pak01_dir.vpk .
